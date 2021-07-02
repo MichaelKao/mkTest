23989395779d1ed23098c25f72317fd00315e6b1
yiching-w <doo8409255@gmail.com>
2021-07-02 15:58:11 +0800

    Issue: Caused by: com.amazonaws.SdkClientException: Unable to find a region via the region provider chain. Must provide an explicit region in the builder or setup environment to supply a region.

--- src/main/java/tw/musemodel/dingzhiqingren/event/SignedUpListener.java
+++ src/main/java/tw/musemodel/dingzhiqingren/event/SignedUpListener.java
@@ -1,5 +1,8 @@
 package tw.musemodel.dingzhiqingren.event;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
@@ -21,7 +24,19 @@ public class SignedUpListener implements ApplicationListener<SignedUpEvent> {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(SignedUpListener.class);
 
-	private static final AmazonSNS AMAZON_SNS = AmazonSNSClientBuilder.defaultClient();
+	private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
+
+	private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
+
+	private static final AmazonSNS AMAZON_SNS = AmazonSNSClientBuilder.
+		standard().
+		withCredentials(new AWSStaticCredentialsProvider(
+			new BasicAWSCredentials(
+				AWS_ACCESS_KEY_ID,
+				AWS_SECRET_ACCESS_KEY
+			)
+		)).
+		withRegion(Regions.AP_SOUTHEAST_1).build();
 
 	@Autowired
 	private LoverService loverService;
