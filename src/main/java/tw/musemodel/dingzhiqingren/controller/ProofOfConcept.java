c741462c9762558af5a1d874058d210b2b7a6cd3
yiching-w <doo8409255@gmail.com>
2021-07-20 11:07:57 +0800

    Issue: 上傳失敗找問題

--- src/main/java/tw/musemodel/dingzhiqingren/controller/ProofOfConcept.java
+++ src/main/java/tw/musemodel/dingzhiqingren/controller/ProofOfConcept.java
@@ -1,9 +1,31 @@
 package tw.musemodel.dingzhiqingren.controller;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import javax.xml.parsers.ParserConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+import tw.musemodel.dingzhiqingren.service.AmazonWebServices;
+import tw.musemodel.dingzhiqingren.service.Servant;
 
 /**
  * 控制器：概念验证
@@ -15,4 +37,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProofOfConcept {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(ProofOfConcept.class);
+
+	@Autowired
+	private AmazonWebServices amazonWebServices;
+
+	@Autowired
+	private Servant servant;
+
+	private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
+
+	private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
+
+	private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
+
+	private static final AmazonS3 AMAZON_S3 = AmazonS3ClientBuilder.
+		standard().
+		withCredentials(new AWSStaticCredentialsProvider(
+			new BasicAWSCredentials(
+				AWS_ACCESS_KEY_ID,
+				AWS_SECRET_ACCESS_KEY
+			)
+		)).withRegion(Regions.AP_SOUTHEAST_1).build();
+
+	private static final String BUCKET_NAME = System.getenv("S3_BUCKET");
+
+	@PostMapping(path = "/upload")
+	@ResponseBody
+	ModelAndView uploadProfileImage(@RequestParam("file") MultipartFile multipartFile, Locale locale) throws SAXException, IOException, ParserConfigurationException {
+
+		File file = new File(TEMP_DIRECTORY, Long.toString(
+			System.currentTimeMillis()
+		));
+		multipartFile.transferTo(file);
+		LOGGER.debug("[上傳]上傳伺服器成功，準備上傳S3。");
+
+		PutObjectResult putObjectResult = AMAZON_S3.putObject(
+			new PutObjectRequest(
+				BUCKET_NAME + "/profileImage",
+				"test",
+				file
+			)
+		);
+		LOGGER.debug("[上傳]上傳S3成功\n{}", putObjectResult);
+		file.delete();
+		LOGGER.debug("[上傳]刪除伺服器檔案成功");
+
+		Document document = servant.parseDocument();
+		Element documentElement = document.getDocumentElement();
+
+		ModelAndView modelAndView = new ModelAndView("upload");
+		modelAndView.
+			getModelMap().
+			addAttribute(document);
+		LOGGER.debug("[上傳]上傳{}", modelAndView);
+		return modelAndView;
+	}
 }
---
+++ src/main/resources/templates/upload.xsl
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:output
+		encoding="UTF-8"
+		media-type="text/html"
+		method="html"
+		indent="no"
+		omit-xml-declaration="yes"
+	/>
+
+	<xsl:include href="default.xsl"/>
+
+	<xsl:template match="/">
+		<xsl:text disable-output-escaping="yes">&#60;!DOCTYPE HTML&#62;</xsl:text>
+		<HTML dir="ltr" lang="zh-Hant">
+			<xsl:apply-templates select="document"/>
+		</HTML>
+	</xsl:template>
+
+	<xsl:template match="document">
+		<HEAD>
+			<xsl:call-template name="headMetaTags"/>
+			<TITLE>
+				<xsl:value-of select="@title"/>
+			</TITLE>
+			<xsl:call-template name="headLinkTags"/>
+		</HEAD>
+		<BODY>
+			<DIV class="page-header section-height-100">
+				<DIV class="container">
+
+				</DIV>
+			</DIV>
+			<xsl:call-template name="bodyScriptTags"/>
+		</BODY>
+	</xsl:template>
+</xsl:stylesheet>
