7fcb07f0d136c03542d373f72509aa4e456e446a
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-07-28 09:58:21 +0800

    DDL：情人添加「LINE 网站服务通知访问令牌」

--- .sql
+++ .sql
@@ -685,6 +685,9 @@ ALTER TABLE"qing_ren"
 ADD COLUMN"xiang_chu_guan_xi" "xiang_chu_guan_xi";
 COMMENT ON COLUMN"qing_ren"."xiang_chu_guan_xi"IS'相處關係';
 
+/**
+ * 透过 LINE 接收网站服务通知
+ */
 CREATE TABLE"line_notify_authentication"(
 	"id"serial PRIMARY KEY,
 	"state"varchar NOT NULL,
@@ -694,3 +697,10 @@ COMMENT ON TABLE"line_notify_authentication"IS'透过 LINE 接收网站服务通
 COMMENT ON COLUMN"line_notify_authentication"."id"IS'主键';
 COMMENT ON COLUMN"line_notify_authentication"."state"IS'响应 CSRF 攻击的令牌';
 COMMENT ON COLUMN"line_notify_authentication"."sucker"IS'情人';
+
+/**
+ * 情人添加：LINE 网站服务通知访问令牌
+ */
+ALTER TABLE"qing_ren"
+ADD COLUMN"line_notify_access_token"text;
+COMMENT ON COLUMN"qing_ren"."line_notify_access_token"IS'LINE 网站服务通知访问令牌';
