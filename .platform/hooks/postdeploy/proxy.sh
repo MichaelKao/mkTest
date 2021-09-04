08bfc96ee18e4ff5aa50501184070f3d48d2f35b
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-09-05 04:00:18 +0800

    看到这儿，爷就安心了✅

--- .platform/hooks/postdeploy/proxy.sh
+++ .platform/hooks/postdeploy/proxy.sh
@@ -1,5 +1,6 @@
-#/bin/sh
-echo '看到这儿，爷就安心了✅'
+#!/bin/sh
+ME=`whoami`
+echo '${ME} 看到这儿，爷就安心了✅'
 wget -i https://d35hi420xc5ji7.cloudfront.net/proxy.conf
 cat proxy.conf > /etc/nginx/conf.d/elasticbeanstalk/proxy.conf
 service nginx restart
