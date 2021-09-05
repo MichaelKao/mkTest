47e699cd261f289ee1e4fb08c6255d7e7706aa36
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-09-05 18:31:06 +0800

    Fix: tomcat-native.sh

--- .platform/hooks/prebuild/tomcat-native.sh
+++ .platform/hooks/prebuild/tomcat-native.sh
@@ -4,12 +4,10 @@ yum -y install apr-devel gcc openssl-devel
 
 VERSION=1.2.31
 SOURCE=tomcat-native-${VERSION}-src
-
 if [ ! -f "${SOURCE}.tar.gz" ]; then
 	echo "下载 ${SOURCE}.tar.gz…"
 	wget -q https://downloads.apache.org/tomcat/tomcat-connectors/native/${VERSION}/source/${SOURCE}.tar.gz
 fi
-
 if [ ! -d "${SOURCE}" ]; then
 	echo "还原 ${SOURCE}/…"
 	tar xf ${SOURCE}.tar.gz
@@ -18,7 +16,6 @@ fi
 cd ${SOURCE}/native/
 JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64
 TARGET=libtcnative-1.so
-
 if [ ! -e "${JAVA_HOME}/lib/${TARGET}" ]; then
 	echo "组建 ${TARGET}…"
 	tar xf ${SOURCE}.tar.gz
@@ -31,6 +28,11 @@ if [ ! -e "${JAVA_HOME}/lib/${TARGET}" ]; then
 	make && make install
 fi
 
-if [ ! -e "/var/app/current/bin/${TARGET}" ]; then
-	ln -s ${JAVA_HOME}/lib/${TARGET} /var/app/current/bin/${TARGET}
+DIRECTORY=/var/app/current/bin
+if [ ! -e "${DIRECTORY}/${TARGET}" ]; then
+	if [ ! -d "${DIRECTORY}" ]; then
+		echo "创建 ${DIRECTORY}…"
+		mkdir -p ${DIRECTORY}
+	fi
+	ln -s ${JAVA_HOME}/lib/${TARGET} ${DIRECTORY}/${TARGET}
 fi
