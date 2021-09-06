43c4ffd7d25fcaec6196d5da6a3a734220a51c55
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-09-06 10:44:56 +0800

    POC: platform hooks current directory

--- .platform/hooks/prebuild/environment.sh
+++ .platform/hooks/prebuild/environment.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-ENVIRONMENT=/etc/environment
-cat /dev/null > /etc/environment
-echo "export LANG=en_US.UTF-8" > ${ENVIRONMENT}
-echo "export LC_ALL=en_US.UTF-8" >> ${ENVIRONMENT}
+cat << EOF > /etc/environment
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+EOF
+
+CWD=`pwd`
+TOKEN=YUaagcLPQsSHQULyV9f09yYtgc8ExoxGoMRfq9z3y88
+curl \
+-H "Authorization: Bearer ${TOKEN}" \
+-X POST \
+--data "message=${CWD}" \
+https://notify-api.line.me/api/notify
