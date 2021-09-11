83c98f28fd067a22ce9cbb39dbbbc564c2874563
yiching-w <71913616+yiching-w@users.noreply.github.com>
2021-09-11 17:40:13 +0800

    Fix: 通知的 websocket 可持續連接

--- src/main/resources/static/SCRIPT/websocket.js
+++ src/main/resources/static/SCRIPT/websocket.js
@@ -8,47 +8,54 @@ $(document).ready(function () {
 		console.log("Not support WebSocket");
 	} else {
 		console.log("Support WebSocket");
+		connect();
 	}
 
-	socket = new WebSocket("wss://" + hostName + "/webSocket/" + identifier);
+	function connect() {
 
-	//開啟事件
-	socket.onopen = function () {
-		console.log("WebSocket is open");
-	};
+		socket = new WebSocket("wss://" + hostName + "/webSocket/" + identifier);
 
-	//獲得訊息事件
-	socket.onmessage = function (msg) {
-		$('.toast-body').html(msg.data);
-		var count;
-		var $notify;
-		if (msg.data.slice(0, 5) === 'inbox') {
-			$('.toast-body').html(msg.data.slice(5));
-			$('.toast').toast('show');
-			$notify = $('.inbox');
-		} else {
+		//開啟事件
+		socket.onopen = function () {
+			console.log("WebSocket is open");
+		};
+
+		//獲得訊息事件
+		socket.onmessage = function (msg) {
 			$('.toast-body').html(msg.data);
-			$('.toast').toast('show');
-			$notify = $('.announcement');
-		}
-		// 增加通知數
-		if (isNaN(parseInt($notify.html()))) {
-			count = 1;
-			$notify.attr('style', 'display: inline;');
-		} else {
-			count = parseInt($notify.html()) + 1;
-		}
-		$notify.html(count);
-	};
+			var count;
+			var $notify;
+			if (msg.data.slice(0, 5) === 'inbox') {
+				$('.toast-body').html(msg.data.slice(5));
+				$('.toast').toast('show');
+				$notify = $('.inbox');
+			} else {
+				$('.toast-body').html(msg.data);
+				$('.toast').toast('show');
+				$notify = $('.announcement');
+			}
+			// 增加通知數
+			if (isNaN(parseInt($notify.html()))) {
+				count = 1;
+				$notify.attr('style', 'display: inline;');
+			} else {
+				count = parseInt($notify.html()) + 1;
+			}
+			$notify.html(count);
+		};
 
-	//關閉事件
-	socket.onclose = function () {
-		console.log("WebSocket is close");
-	};
+		//關閉事件
+		socket.onclose = function () {
+			console.log("WebSocket is close");
+			setTimeout(function () {
+				connect();
+			}, 1000);
+		};
 
-	//發生了錯誤事件
-	socket.onerror = function () {
-		console.log("WebSocket is error");
+		//發生了錯誤事件
+		socket.onerror = function () {
+			console.log("WebSocket is error");
+		}
 	}
 
 	// 關閉或離開頁面後關閉連接
