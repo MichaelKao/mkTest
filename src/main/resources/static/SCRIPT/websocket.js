7003d0f95571f09a4c9dd2dcc9fa945b9ab8b768
yiching-w <doo8409255@gmail.com>
2021-09-19 10:02:54 +0800

    Fix: Websocket通知停止自動回復連線

--- src/main/resources/static/SCRIPT/websocket.js
+++ src/main/resources/static/SCRIPT/websocket.js
@@ -5,57 +5,50 @@ $(document).ready(function () {
 
 	var socket;
 	if (typeof (WebSocket) == "undefined") {
-		console.log("[Notify]Not support WebSocket");
+		console.log("Not support WebSocket");
 	} else {
-		console.log("[Notify]Support WebSocket");
-		connect();
+		console.log("Support WebSocket");
 	}
 
-	function connect() {
+	socket = new WebSocket("wss://" + hostName + "/webSocket/" + identifier);
 
-		socket = new WebSocket("wss://" + hostName + "/webSocket/" + identifier);
+	//開啟事件
+	socket.onopen = function () {
+		console.log("WebSocket is open");
+	};
 
-		//開啟事件
-		socket.onopen = function () {
-			console.log("[Notify]WebSocket is open");
-		};
-
-		//獲得訊息事件
-		socket.onmessage = function (msg) {
+	//獲得訊息事件
+	socket.onmessage = function (msg) {
+		$('.toast-body').html(msg.data);
+		var count;
+		var $notify;
+		if (msg.data.slice(0, 5) === 'inbox') {
+			$('.toast-body').html(msg.data.slice(5));
+			$('.toast').toast('show');
+			$notify = $('.inbox');
+		} else {
 			$('.toast-body').html(msg.data);
-			var count;
-			var $notify;
-			if (msg.data.slice(0, 5) === 'inbox') {
-				$('.toast-body').html(msg.data.slice(5));
-				$('.toast').toast('show');
-				$notify = $('.inbox');
-			} else {
-				$('.toast-body').html(msg.data);
-				$('.toast').toast('show');
-				$notify = $('.announcement');
-			}
-			// 增加通知數
-			if (isNaN(parseInt($notify.html()))) {
-				count = 1;
-				$notify.attr('style', 'display: inline;');
-			} else {
-				count = parseInt($notify.html()) + 1;
-			}
-			$notify.html(count);
-		};
-
-		//關閉事件
-		socket.onclose = function () {
-			console.log("[Notify]WebSocket is close");
-			setTimeout(function () {
-				connect();
-			}, 1000);
-		};
-
-		//發生了錯誤事件
-		socket.onerror = function () {
-			console.log("[Notify]WebSocket is error");
+			$('.toast').toast('show');
+			$notify = $('.announcement');
 		}
+		// 增加通知數
+		if (isNaN(parseInt($notify.html()))) {
+			count = 1;
+			$notify.attr('style', 'display: inline;');
+		} else {
+			count = parseInt($notify.html()) + 1;
+		}
+		$notify.html(count);
+	};
+
+	//關閉事件
+	socket.onclose = function () {
+		console.log("WebSocket is close");
+	};
+
+	//發生了錯誤事件
+	socket.onerror = function () {
+		console.log("WebSocket is error");
 	}
 
 	// 關閉或離開頁面後關閉連接
