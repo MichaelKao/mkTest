dded5c9480d673c12f136d7183a81ac075953a56
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-09-22 17:21:53 +0800

    优化：删除 IDE 告警

--- src/main/java/tw/musemodel/dingzhiqingren/controller/LineNotifyController.java
+++ src/main/java/tw/musemodel/dingzhiqingren/controller/LineNotifyController.java
@@ -8,7 +8,6 @@ import me.line.notifybot.oauth.AuthorizationCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
