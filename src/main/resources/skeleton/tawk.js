cf222265af1d8df50cff00fe618dde6e67120151
P-C Lin (a.k.a 高科技黑手 <p@w3c.org.tw>
2021-11-02 15:31:54 +0800

    Feature: Embed inline tawk.to

---
+++ src/main/resources/skeleton/tawk.js
@@ -0,0 +1,1 @@
+var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();(function(){var s1=document.createElement('SCRIPT'),s0=document.getElementsByTagName("SCRIPT")[0];s1.async=true;s1.src='https://embed.tawk.to/%s';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();
