5d2e961d4edd15ef7a0db2e543f539d13fac0f04
yiching-w <71913616+yiching-w@users.noreply.github.com>
2022-01-12 09:20:29 +0800

    Feature: 新增 Apple Pay 商店網域驗證檔案

--- src/main/resources/static/SCRIPT/rechargeByInpay2_.js
+++ src/main/resources/static/SCRIPT/rechargeByInpay2_.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
 					}
 				},
 				'json'
-			);
+				);
 		}
 	);
 
@@ -49,28 +49,28 @@ $(document).ready(function () {
 				let payToken = paymentInfo.PayToken;
 				$(form.payToken).val(
 					payToken
-				);
+					);
 				$(form.paymentType).val(
 					paymentInfo.PaymentType
-				);
+					);
 				$('DIV.loadingWrap').css('display', 'block');
 				$.post(
 					`/inpay2/createPayment/${payToken}.json`,
 					function (data) {
 						console.log(data);
-                                                if (data.ThreeDInfo.ThreeDURL) {
-                                                        location.replace(data.ThreeDInfo.ThreeDURL);
+						if (data.ThreeDInfo.ThreeDURL) {
+							location.replace(data.ThreeDInfo.ThreeDURL);
 						} else if (data.CVSInfo.PaymentURL) {
 							location.replace(data.CVSInfo.PaymentURL);
 						} else if (data.ATMInfo.BankCode && data.ATMInfo.vAccount && data.ATMInfo.ExpireDate) {
 							alert(`繳費銀行代碼：${data.ATMInfo.BankCode}\n繳費虛擬帳號：${data.ATMInfo.vAccount}\n繳費期限：${data.ATMInfo.ExpireDate}`);
 						} else {
 							alert(`{"RtnCode":${data.RtnCode},"RtnMsg":"${data.RtnMsg}"}`);
-                                                }
+						}
 						$('DIV.loadingWrap').css('display', 'none');
 					},
 					'json'
-				);
+					);
 			});
 		} catch (error) {
 			alert(error);
